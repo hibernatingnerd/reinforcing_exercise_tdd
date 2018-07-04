0df30ffd36da655585c161f6b88f12603072ff24
hibernatingnerd <jeffery.mark.campbell@gmail.com>
2018-07-04 11:53:08 -0400

    finish reinforcing exercise

---
+++ calculator.rb
@@ -0,0 +1,15 @@
+
+  def add(num1, num2)
+    sum = num1 + num2
+    return sum
+  end
+
+  def subtract(num1, num2)
+    sum = num1 - num2
+    return sum
+  end
+
+  def sum(array)
+    sum = array.sum
+    return sum
+  end
